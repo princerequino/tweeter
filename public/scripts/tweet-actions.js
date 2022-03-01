5eed7a9b17a1540dacf7a4fef0bcff64738b366d
princerequino <requino.prince@gmail.com>
2022-03-01 16:21:27 -0500

    fix hover colors

--- public/scripts/tweet-actions.js
+++ public/scripts/tweet-actions.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
   $(".posted-tweet .tweet-footer span").text(timeago.format(new Date()));
 
 
-  // $(".fas").hover(function() {
-  //   $(this).css("color", "gold");
-  // }, function(){
-  //   $(this).css("color", "darkblue")
-  // })
+  $(".fas").hover(function() {
+    $(this).css("color", "green");
+  }, function(){
+    $(this).css("color", "black")
+  })
 
   // $(".posted-tweet div").hover(function() {
   //   $(this).css("box-shadow", "3px 3px");
