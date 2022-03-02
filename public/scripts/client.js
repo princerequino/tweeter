9eeb6a8d02f1b6b0bb5afd2261fbf8aee17cac4a
princerequino <requino.prince@gmail.com>
2022-03-02 18:56:34 -0500

    code refactor

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,6 @@ const renderTweets = (tweetData) => {
   }
 }
 
-//Receives an obj and renders HTML  
-const createTweetElement = (tweet) => {
-  
-  const handle = tweet.user.handle;
-  const tweetText = tweet.content.text;
-  const date = timeago.format(tweet.created_at);
-  const $tweetHTML = `
-  <article name="tweet">
-  <div class="posted-tweet">
-  <header>
-    <img src="${avatar}">
-    <h4> ${user} <span>${handle}</span></h4>
-    ${tweetText}
-  </header>
-  <footer>
-    <span id="time">${date}</span>
-    <span><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
-  </footer>
-</div>
-</article>`;
-
-return $tweetHTML;
-}
-
-
 //Function for loading all the tweets from database
 const loadtweets = function () {
   $.ajax(`/tweets`, { method: 'GET' }).then(function (tweets) {
