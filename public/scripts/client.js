134790fc0fb8d3942944c2dca1cf650a77c959eb
princerequino <requino.prince@gmail.com>
2022-03-02 17:07:47 -0500

    add init data, delete html and timeago using CDN

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,53 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+//Iterates through the array of data
+const renderTweets = (tweetData) => {
+  for (const obj of tweetData) {
+    let newTweet = createTweetElement(obj);
+    $(".posts").prepend(newTweet);
+  }
+}
 
+//Receives an obj and renders HTML
+const createTweetElement = (tweet) => {
+  const user = tweet.user.name;
+  const avatar = tweet.user.avatars;
+
+    
+        
+          
+    
+
+        
+    
+const createTweetElement = (tweet) => {
+  
+  const handle = tweet.user.handle;
+  const tweetText = tweet.content.text;
+  const date = timeago.format(tweet.created_at);
+  const $tweetHTML = `
+  <div class="posted-tweet">
+  <header>
+    <img src="${avatar}">
+    <h4> ${user} <span>${handle}</span></h4>
+    <article name="tweet">${tweetText}</article>
+  </header>
+  <footer>
+    <span id="time">${date}</span>
+    <span><i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i></span>
+  </footer>
+</div>`;
+return $tweetHTML;
+}
+
+  $(".posted-tweet").append($tweetHTML);
+};
+
+//Function for loading all the tweets from database
+const loadtweets = function () {
+  $.ajax(`/tweets`, { method: 'GET' }).then(function (tweets) {
+    renderTweets(tweets);
+  })
+}
+
+$(document).ready(function () {
+  loadtweets();
+});
--- public/scripts/tweet-actions.js
+++ public/scripts/tweet-actions.js
@@ -1,17 +1,69 @@
-$(document).ready(function() {
-  $(".posted-tweet .tweet-footer span").text(timeago.format(new Date()));
+$(document).ready(function () {
+  const initData = {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1628546254537
+};
+  //Function to create HTML code for new tweet
+  const createTweetElement = (tweet) => {
+    const user = tweet.user.name;
+    const avatar = tweet.user.avatars;
+    const handle = tweet.user.handle;
+    const tweetText = tweet.content.text;
+    const date = timeago.format(tweet.created_at);
+
+    const $tweetHTML = `
+    <section class="posted-tweet">
+    <div class="tweet-header">
+    <div class="photo-user">
+      <img src="${avatar}">
+      <h4> ${user}</h4>
+      </div>
+      <span>${handle}</span>
+      </div>
+      <div class="tweet-body" name="tweet">${tweetText}
+      </div>
+    <div class="tweet-footer">
+      <span id="time">${date}</span>
+      <div class="icons">
+  <i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i>
+    </div>
+    </div>
+  </section>`;
+
+    return $tweetHTML;
+  }
+
+  //Listening to submit requests
+  $(`#tweet-form`).submit(function (ev) {
+    ev.preventDefault();
+    const serial = $('#tweet-form').serialize();
+    const tweetLength = serial.split("=").pop().length;
 
 
-  $(".fas").hover(function() {
-    $(this).css("color", "green");
-  }, function(){
-    $(this).css("color", "black")
-  })
+    if (tweetLength > 140) { //will return error if characters exceed 140
+      alert("Too many characters my friend!");
+      return;
+    } else { //will add new tweet to database and prepend to page
+      $.post(`/tweets/`, serial, null).then(function () {
+        $.ajax(`/tweets`, { method: 'GET' }).then(function (tweet) {
 
-  $(".posted-tweet").hover(function() {
-    $(this).css("box-shadow", "3px 3px");
-  }, function(){
-    $(this).css("box-shadow", "0px 0px");
-  })
+          let $newHTML = createTweetElement(tweet[tweet.length - 1]);
+          $(`${$newHTML}`).prependTo($('.posts')).hide().slideDown("slow");
+        })
+      })
+    }
+
+  });
+
+  const $newTweet = createTweetElement(initData);
+
+  $('.posts').append($newTweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 });
