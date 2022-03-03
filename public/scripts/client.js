225008305a00ce57786f1568a17941080760ccfc
princerequino <requino.prince@gmail.com>
2022-03-02 19:12:12 -0500

    refactor code // avoid duplication // remove Hard-coded data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,35 @@
+  //Function to create HTML code for new tweet
+  const createTweetElement = (tweet) => {
+    const user = tweet.user.name;
+    const avatar = tweet.user.avatars;
+    const handle = tweet.user.handle;
+    const tweetText = tweet.content.text;
+    const date = timeago.format(tweet.created_at);
+
+    const $tweetHTML = `
+    <section class="posted-tweet">
+    <div class="tweet-header">
+    <div class="photo-user">
+      <img src="${avatar}">
+      <h4> ${user}</h4>
+      </div>
+      <span>${handle}</span>
+      </div>
+      <div class="tweet-body" name="tweet">${escape(tweetText)}
+      </div>
+    <div class="tweet-footer">
+      <span id="time">${date}</span>
+      <div class="icons">
+  <i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i>
+    </div>
+    </div>
+  </section>`;
+
+    return $tweetHTML;
+  }
+
+
+
 //Iterates through the array of data
 const renderTweets = (tweetData) => {
   for (const obj of tweetData) {
@@ -17,6 +49,8 @@ $(document).ready(function () {
   loadtweets();
 });
 
+
+// ESCAPE FUNCTION TO PREVENT XSS
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
--- public/scripts/tweet-actions.js
+++ public/scripts/tweet-actions.js
@@ -1,46 +1,4 @@
 $(document).ready(function () {
-
-  const initData = {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1628546254537
-};
-  //Function to create HTML code for new tweet
-  const createTweetElement = (tweet) => {
-    const user = tweet.user.name;
-    const avatar = tweet.user.avatars;
-    const handle = tweet.user.handle;
-    const tweetText = tweet.content.text;
-    const date = timeago.format(tweet.created_at);
-
-    const $tweetHTML = `
-    <section class="posted-tweet">
-    <div class="tweet-header">
-    <div class="photo-user">
-      <img src="${avatar}">
-      <h4> ${user}</h4>
-      </div>
-      <span>${handle}</span>
-      </div>
-      <div class="tweet-body" name="tweet">${escape(tweetText)}
-      </div>
-    <div class="tweet-footer">
-      <span id="time">${date}</span>
-      <div class="icons">
-  <i class="fas fa-flag"></i><i class="fas fa-retweet"></i><i class="fas fa-heart"></i>
-    </div>
-    </div>
-  </section>`;
-
-    return $tweetHTML;
-  }
-
   //Listening to submit requests
   $(`#tweet-form`).submit(function (ev) {
     ev.preventDefault();
@@ -68,11 +26,5 @@ $(document).ready(function () {
     
   });
 
-
-  
-
-  const $newTweet = createTweetElement(initData);
-
-  $('.posts').append($newTweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 });
